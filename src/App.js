920b1bc1b1a6a27c91ce9bb0bfa6ee829ed812ca
thainq-teko <thai.nq@teko.vn>
2019-07-05 11:57:20 +0700

    useEffect => buggy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'reactn';
 import './App.css';
 import SimpleSwiperWithParams from './components/slider'
 import "react-id-swiper/lib/styles/scss/swiper.scss";
 import axios from 'axios'
 
-const getData = async () => {
-  const resp = await axios.get('https://phongvu.vn/api-v2/banner/gethomepagebannergroup?position=hp-de-slider-660x390&category=home&version=1')
-  let temp = resp.data.data
-  let array = []
-  temp.forEach(i => {
-    array = array.concat(i.images)
-  });
-  console.log(array)
-  return array
-}
-getData()
+
 function App() {
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+    const resp = await axios.get('https://phongvu.vn/api-v2/banner/gethomepagebannergroup?position=hp-de-slider-660x390&category=home&version=1')
+    let temp = resp.data.data
+    let array = []
+    temp.forEach(i => {
+      array = array.concat(i.images)
+    });
+    setData(array)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <h1>Header</h1>
-      <br/>
-      <SimpleSwiperWithParams props={getData()}/>
+      <br />
+      <div>
+        <SimpleSwiperWithParams data={data} />
+      </div>
     </div>
   );
 }
--- src/components/slider.js
+++ src/components/slider.js
@@ -1,15 +1,16 @@
 import React from 'reactn';
-import { useGlobal } from 'reactn'
 import Swiper from 'react-id-swiper';
+import { Autoplay, Pagination } from 'swiper/dist/js/swiper.esm'
 
-import slide1 from '../assets/slide1.jpg';
-import slide2 from '../assets/slide2.jpg';
-
-const data = useGlobal("arrayList")
-
-const SimpleSwiperWithParams = (props) => {
-  console.log(props)
+const SimpleSwiperWithParams = props => {
+  const { data } = props
+  console.log(data)
   const params = {
+    modules: [Autoplay, Pagination],
+    autoplay: {
+      delay: 2500
+    },
+    grabCursor: true,
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
@@ -23,23 +24,18 @@ const SimpleSwiperWithParams = (props) => {
     centeredSlides: true,
   }
 
+  const createSlide = list => {
+    let div = []
+    list.forEach(i => {
+      div.push(<div><img alt={i.name} className="swiper-img" src={i.image_src} key={i.promo_id}></img></div>)
+    })
+    return div
+  }
+
   return (
     <Swiper {...params}>
-      <div>
-        <img src={slide1} alt="slide1" style={{textAlign: "center"}} />
-      </div>
-      <div>
-        <img src={slide2} alt="slide1" />
-      </div>
-      <div>
-        <img src={slide1} alt="slide1" />
-      </div>
-      <div>
-        <img src={slide2} alt="slide1" />
-      </div>
-      <div>
-        <img src={slide1} alt="slide1" />
-      </div>
+      {createSlide(data)}
+      {/* <img src={data[0].image_src} alt=""/> */}
     </Swiper>
   )
 }
