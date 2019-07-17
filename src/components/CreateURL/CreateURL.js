56b439ac12730e25c39170ab08a8e883605b99fd
thainq-teko <thai.nq@teko.vn>
2019-07-17 17:44:38 +0700

    add API calling

--- src/components/CreateURL/CreateURL.js
+++ src/components/CreateURL/CreateURL.js
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './CreateURL.scss'
 import SharedInput from './SharedInput/SharedInput'
 import { Button, Select } from 'antd';
-import { optionalCallExpression } from '@babel/types';
-// import axios from 'axios'
+import axios from 'axios'
 
-// const result = axios.get('https://catalog-dev.phongvu.vn/extra-data')
-// console.log(result)
 const { Option } = Select;
 const CreateURL = props => {
   const [input, setInput] = useState([])
   const [id_u, setId_u] = useState(0)
   const [dieukienloc, setDieukienloc] = useState([])
-  const [option, setOption] = useState([])
+  const [attributeSets, setAttributeSets] = useState([])
   const tieuchi = [
     {
       id: 0,
@@ -39,7 +36,13 @@ const CreateURL = props => {
       name: 'Giá',
     },
   ]
-  useEffect(() => console.log(input), [input])
+  
+  useEffect(() => {    
+    axios.get('https://catalog-dev.phongvu.vn/extra-data')
+    .then(res => setAttributeSets(res.data.attribute_sets))
+  }, [])
+  console.log(attributeSets)
+  useEffect(() => console.log("dk loc:", dieukienloc))
   const handleAddingNewRow = id_u => {
     let temp = id_u + 1
     setId_u(temp)
@@ -49,28 +52,33 @@ const CreateURL = props => {
     } }])
   }
   const handleMinus = (index) => {
-    console.log("vi tri xoa: ", index)
     setInput(input.filter((element) => element.id !== index))
   }
   const handleTieuchilocChange = (event, id) => {
-    console.log(event, id)
     let input1 = []
     input.forEach(el => input1.push(el))
     input1.forEach(element => {
-      if (element.id === id) element.data.field1 = event
+      if (element.id === id) {
+        element.data.field1 = event
+      }
     })
     setInput(input1)
-    // // call API
-    setDieukienloc(dieukienloc.concat(event))
+    // call API
+    if (event === "Bộ thuộc tính") {
+      setDieukienloc(attributeSets)
+    }
+  }
+  const handleChange = (value) => {
+    console.log(value)
   }
   const divA = (element) => (
-    <div>
+    <div key={element.id}>
       <Button type="dangerous" shape="circle" icon="minus" onClick={() => handleMinus(element.id)} />
       <Select className="w-20" value={element.data.field1} placeholder="Chọn tiêu chí lọc" onChange={(event) => handleTieuchilocChange(event, element.id)}>
         {tieuchi.map(item => <Option key={item.id} value={item.name}>{item.name}</Option>)}
       </Select>
-      <Select className="w-20" value={element.data.field2}>
-        {dieukienloc.map(item => <Option key={item} value={item}>{item}</Option>)}
+      <Select className="w-20" placeholder="Chọn điều kiện lọc" mode="multiple" onChange={handleChange}>
+        {dieukienloc && dieukienloc.map(item => <Option key={item.name}>{item.name}</Option>)}
       </Select>
       <br />
     </div>)
